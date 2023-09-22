63ba41d49fb6222500c3951f22f501d6a9d88b15
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-09-22 16:46:56 +0200

    folder name changed to bff (#18)
    * folder name changed to bff
    
    The BFF is only a backend for frontend, not a true backend, changed foldername to reflect this.
    
    * HomeController and ProfileController initial setup

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Authentication.UI.Controllers
+{
+    [ApiController]
+    [AutoValidateAntiforgeryToken] //TODO: bruke Filteret til AM, prøver uten først så Torgeir får sin Mocked BFF
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAntiforgery _antiforgery;
+
+        public AuthenticationController(IAntiforgery antiforgery)
+        {
+            _antiforgery = antiforgery;
+        }
+
+        [Authorize]
+        //[HttpGet("authfront/api/v1/authentication/refresh")]        //the correct URL
+        [HttpGet("accessmanagement/api/v1/authentication/refresh")]   //this is the URL the js tries to hit now, must be changed
+        public async Task<IActionResult> Refresh()
+        {
+            AntiforgeryTokenSet tokens = _antiforgery.GetAndStoreTokens(HttpContext);
+            HttpContext.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken, new CookieOptions
+            {
+                Domain = "localhost",
+                HttpOnly = true,
+                Secure = true,
+                IsEssential = true,
+                SameSite = SameSiteMode.Lax
+            });
+
+            return Ok();
+
+        }
+    }
+}
---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/ProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Altinn.Authentication.UI.Controllers
+{
+    [Route("authfront/api/v1/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        //private readonly IProfileClient _profileClient;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProfileController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<ActionResult> GetUser()
+        {
+
+        }
+    }
+}
