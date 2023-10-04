91fbc73a58ce8ace84f6e5bae07f03cf30be3db1
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-10-04 12:22:48 +0200

    Fixed User DTO (#36)
    Fixed User DTO to contain better data for testing of the Frontend.

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/ProfileController.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/ProfileController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Altinn.Platform.Profile.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Altinn.Authentication.UI.Controllers
 {
+    /// <summary>
+    /// The <see cref="ProfileController"/> provides the API endpoints related to persons.
+    /// </summary>
     [Route("authfront/api/v1/profile")]
     [ApiController]
     public class ProfileController : ControllerBase
@@ -11,31 +15,42 @@ namespace Altinn.Authentication.UI.Controllers
         //private readonly IProfileClient _profileClient;
         private readonly IHttpContextAccessor _httpContextAccessor;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfileController"/> class
+        /// </summary>
         public ProfileController(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
         }
 
+        /// <summary>
+        /// Method that returns the user information about the user that is logged in
+        /// <param name = "UserDTO">The UserProfile as a DTO for the Frontend</param>
+        /// </summary>
         //[Authorize] //TODO: må fikse
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         [HttpGet("user")]
         public async Task<ActionResult> GetUser()
         {
-            var user = new
+
+            UserProfile user = new()
             {
                 UserId = 20004938,
-                UserName = "JarleErKul",
-                PhoneNumber = "90001337",
                 Email = "1337@altinnstudiotestusers.com",
+                PhoneNumber = "90001337",
+                UserName = "Testur Testursson",
                 PartyId = 50019992,
-                Party = new
+                ExternalIdentity = "",
+                Party = new Platform.Register.Models.Party 
                 {
-                    UserType = 1,
-                    ProfileSettingPreference = new
-                    {
-                        Language = "nb"
-                    }
-                }
+                    Name = "TestParty"
+                },
+                ProfileSettingPreference = new() 
+                {
+                    Language = "nb"
+                },
+                UserType = Platform.Profile.Enums.UserType.SSNIdentified             
+
             };
 
             return Ok(user);
