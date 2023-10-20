a736f00c59abf1b84c609a3f81d1a7f4f5b6ac58
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-10-20 09:49:57 +0200

    Sr system user 5 (#60)
    * housekeeping
    
    * extensions and helpers
    
    Added some extensions, helpers and utils, and restructured some namspaces to improve readability
    
    * AuthenticationHelper
    
    * More Authentication helpers
    
    * Configurations
    
    * Upload Jwk with Metadata

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/AppConfiguration/ClientSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Authentication.UI.Core.AppConfiguration;
+
+public class ClientSettings
+{
+    public ClientSettings(string issuer, string app, string certificateName)
+    {
+        Issuer = issuer;
+        App = app;
+        CertificateName = certificateName;
+    }
+
+    public string Issuer { get; set; }
+
+    public string App { get; set; }
+
+    public string CertificateName { get; set; }
+}
---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/Authentication/JwtTokenUtil.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Altinn.Authentication.UI.Core.Authentication;
+
+/// <summary>
+/// Retrieves Cookie or Bearer token from the Context
+/// </summary>
+public static class JwtTokenUtil
+{
+    public static string? GetTokenFromContext(HttpContext context, string cookiename)
+    {
+        //Get the cookie from the request
+        string? token = context.Request.Cookies[cookiename];
+
+        //If there is no cookie, search for a Bearer token
+        if (token is null || token == string.Empty)
+        {
+            string? authorization = context.Request.Headers["Authorization"];
+            if (authorization is null || authorization == string.Empty)
+            {
+                return string.Empty;
+            }
+
+            if (authorization.StartsWith("Bearer ", StringComparison.OrdinalIgnoreCase))
+            {
+                token = authorization["Bearer ".Length..].Trim();
+            }
+        }
+
+        return token;
+    }
+
+    /// <summary>
+    /// Update the http client with new Authorization Bearer token in request header
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="token"></param>
+    public static void ReAddTokenToRequestHeader(HttpClient client, string token)
+    {
+        if (client.DefaultRequestHeaders.Contains("Authorization"))
+        {
+            client.DefaultRequestHeaders.Remove("Authorization");
+        }
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+    }
+
+}
+
