8fe605e684db05fc98ffa6eba83d33ab5c32fa45
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-10-24 10:22:21 +0200

    Update ProfileController.cs

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/ProfileController.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/ProfileController.cs
@@ -2,8 +2,8 @@
 using Altinn.Platform.Profile.Models;
 using Microsoft.AspNetCore.Mvc;
 using Altinn.Authentication.UI.Models;
-using System.Runtime.CompilerServices;
 using Altinn.Authentication.UI.Core.UserProfiles;
+using Altinn.Authentication.UI.Core.Authentication;
 
 namespace Altinn.Authentication.UI.Controllers;
 
@@ -33,19 +33,37 @@ public class ProfileController : ControllerBase
     /// Method that returns the user information about the user that is logged in
     /// <param name = "UserDTO">The UserProfile as a DTO for the Frontend</param>
     /// </summary>
-    //[Authorize] 
+    [Authorize] 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [HttpGet("user")]
     public async Task<ActionResult> GetUser()
     {
+        UserNameAndOrganizatioNameDTO? userDTO;
 
-        UserProfile user = await _userProfileService.GetUserProfile(1);
+        var context = _httpContextAccessor.HttpContext;
+        if (context is null) return StatusCode(500);
 
-        UserNameAndOrganizatioNameDTO userDTO = new() 
-        { 
-            UserName = user.UserName,
-            OrganizationName = user.Party.Name
-        };
+        int userid = AuthenticationHelper.GetUserId(context);
+            if(userid == 0 )return BadRequest("Userid not provided in the context.");
+
+        try
+        {
+            UserProfile user = await _userProfileService.GetUserProfile(1);
+            if (user is null) return NotFound();
+            
+            userDTO = new()
+            {
+                UserName = user.UserName,
+                OrganizationName = user.Party.Name
+            };
+
+        }
+        catch (Exception e)
+        {
+            return StatusCode(500, e.Message);
+        }
+
+        
 
         return Ok(userDTO);
     }
