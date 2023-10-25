9bcec446652d297574eeada9fb3b6637f9cd15b2
Torgeir Holen <torgeir333@gmail.com>
2023-10-25 14:13:54 +0200

    first draft of reorganized creationPageSlice - not functional yet

--- frontend/src/rtk/features/creationPage/creationPageSlice.ts
+++ frontend/src/rtk/features/creationPage/creationPageSlice.ts
@@ -1,7 +1,14 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+// FIX-ME: må omskrives slik at navn, beskrivelse og selected
+// blir tilgjengelig/dispatch i det fokus mistes, a la MaskinportenPage
+// ---> Opprett-knapp bør også inaktiveres til de 3 datapunktene er klare
+
+// slik det er i dag så er det en knapp som flytter data til Redux...
 
 export interface SliceState {
-    navn: string;
+  navn: string;
   beskrivelse: string;
   selected: string;
 }
@@ -11,6 +18,30 @@ const initialState: SliceState = {
     beskrivelse: '', 
     selected: '',
 };
+
+// CreationShape form is based on Swagger POST description per 25.10.23
+export interface CreationRequest {
+  integrationTitle: string,
+  description: string,
+  selectedSystemType: string,
+  clientId: string,
+  ownedByPartyId: string
+}
+
+// temporary form of post call: based on postApiDelegation in AccMan
+// and Swagger POST /authfront/api/v1/systemuser
+// problem is that Request body in Swagger requires 5 key:value pairs
+// while we only have navn, beskrivelse and selected so far...
+export const postNewSystemUser = createAsyncThunk('creationPageSlice/postNewSystemUser', 
+  async (systemUserInfo: CreationRequest) => {
+  return await axios
+    .post('/authfront/api/v1/systemuser', systemUserInfo)
+    .then((response) => response.data)
+    .catch((error) => {
+      console.error(error);
+      throw new Error(String(error.response.data));
+    });
+});
 
 // foreløpig lagreOpprettKnapp skal senere gi API POST kall til BFF
 const creationPageSlice = createSlice({
@@ -22,6 +53,16 @@ const creationPageSlice = createSlice({
         state.beskrivelse = action.payload.beskrivelse;
         state.selected = action.payload.selected;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(postNewSystemUser.fulfilled, (state, action) => {
+        state.systemUserArray = downLoadedArray;
+        state.overviewLoaded = true;
+      })
+      .addCase(postNewSystemUser.rejected, (state, action) => {
+        state.overviewError = action.error.message ?? 'Unknown error';
+      })
   },
 });
 
