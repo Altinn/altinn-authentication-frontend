9877d972c7906bc07d04f991efbc4f43d156dfbf
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-10-30 12:16:04 +0100

    HealthChecks and HealthChcekTest

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Tests/Health/HealthCheckTest.cs
@@ -0,0 +1,30 @@
+﻿using Altinn.Authentication.UI.Health;
+using Altinn.Authentication.UI.Tests.Utils;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using System.Net;
+using Xunit;
+
+namespace Altinn.Authentication.UI.Tests;
+
+public class HealthCheckTest :IClassFixture<CustomWebApplicationFactory<HealthCheck>>
+{
+    private readonly CustomWebApplicationFactory<HealthCheck> _factory;
+
+    public HealthCheckTest(CustomWebApplicationFactory<HealthCheck> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task VerifyHealthCheck_OK()
+    {
+        HttpClient client = SetupUtils.GetTestClient(_factory, false);
+        HttpRequestMessage request = new(HttpMethod.Get, $"/health");
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        string result = await response.Content.ReadAsStringAsync();
+        Assert.Equal(HealthStatus.Healthy.ToString() ,result);
+    }
+}
