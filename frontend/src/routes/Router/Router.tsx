199dda50738711460a0bfd0ea6859bf9094a8ffd
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2023-11-22 14:14:51 +0100

    Update Router.tsx

--- frontend/src/routes/Router/Router.tsx
+++ frontend/src/routes/Router/Router.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 import * as React from 'react';
 
-import { OverviewPage } from '@/features/overviewPage/OverviewPage';
-import { CreationPage } from '@/features/creationPage/CreationPage';
-import { DirectConsentPage } from '@/features/directConsentPage/DirectConsentPage';
+import { OverviewPage } from '@/features/overviewpage/OverviewPage';
+import { CreationPage } from '@/features/creationpage/CreationPage';
+import { DirectConsentPage } from '@/features/directconsentpage/DirectConsentPage';
 import { MaskinportenAdmPage } from '@/features/maskinportenAdm/MaskinportenAdmPage';
 
 import { NotFoundSite } from '@/sites/NotFoundSite';
