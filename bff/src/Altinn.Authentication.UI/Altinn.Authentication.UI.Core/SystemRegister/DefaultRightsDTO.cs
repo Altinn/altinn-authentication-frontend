51cf8b12a7236a631817a5b36745e349d35b29a2
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-01-09 14:45:05 +0100

    New API endpoint for Default Rights

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemRegister/DefaultRightsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Altinn.Authentication.UI.Core.SystemRegister
+{
+    public class DefaultRightsDTO
+    {
+        public string Right { get; set; } = string.Empty;
+        public string ServiceProvider { get; set; } = string.Empty;
+    }
+}
--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/SystemRegisterController.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Controllers/SystemRegisterController.cs
@@ -24,8 +24,21 @@ public class SystemRegisterController : ControllerBase
     {
         List<RegisteredSystemDTO> lista = new();
 
-        lista.AddRange( await _systemRegisterService.GetListRegSys(cancellationToken));
+        lista.AddRange(await _systemRegisterService.GetListRegSys(cancellationToken));
 
+        return Ok(lista);
+    }
+
+    [HttpGet("product/{productId}")]
+    public async Task<ActionResult> GetDefaultRightsForProductName(string productId, CancellationToken cancellationToken = default)    
+    {
+        List<DefaultRightsDTO> lista = new();
+        DefaultRightsDTO l1 = new() { Right = "Mva Registrering", ServiceProvider = "Skatteetaten" };
+        DefaultRightsDTO l2 = new() { Right = "Lønns Rapportering", ServiceProvider = "Skatteetaten" };
+        DefaultRightsDTO l3 = new() { Right = "Lakselus Rapportering", ServiceProvider = "Mattilsynet" };
+        lista.Add(l1);
+        lista.Add(l2);
+        lista.Add(l3);
         return Ok(lista);
     }
 }
