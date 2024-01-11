7f539031fe09336a1fa5cb70a1e7068df850fce2
Torgeir Holen <94476391+Torgeir333@users.noreply.github.com>
2024-01-11 15:16:04 +0100

    First iteration at collection array-of-rights inside CollectionBar array on OverviewPage (#147)
    * set up ActionBar array inside CollectionBar array
    
    * hard code CollectionBar message
    
    * remove button inside ActionBar array

--- frontend/src/features/overviewpage/OverviewPageContent.tsx
+++ frontend/src/features/overviewpage/OverviewPageContent.tsx
@@ -4,8 +4,9 @@ import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '@/rtk/app/hooks';
 import { AuthenticationPath } from '@/routes/paths';
 import classes from './OverviewPageContent.module.css';
-import { CollectionBar } from '@/components';
+import { CollectionBar, ActionBar } from '@/components';
 import { ReactComponent as Add } from '@/assets/Add.svg';
+import { MinusCircleIcon } from '@navikt/aksel-icons';
 import { Button } from '@digdir/design-system-react';
 import { useMediaQuery } from '@/resources/hooks';
 import { useTranslation } from 'react-i18next';
@@ -33,9 +34,50 @@ export const OverviewPageContent = () => {
   const { t } = useTranslation('common'); // not used yet
   const navigate = useNavigate();
 
-  // Fix-me: additionalText prop into CollectionBar is removed in Design of 24.11.23:
-  // set to empty string for now: if this persists the props should be reorganized
 
+  // Experiment 11.01.24: CollectionBar should have collection: experiment with
+  // AccMan repo: /src/features/singleRight/components/ResourceCollectionBar
+
+  // this functional component selectedResourcesActionBars
+  // is fed into the collection prop of CollectionBar, which I have
+  // left empty so far. I hack resources to the new rights-array in Redux:
+  // systemRegisterProductsArray: used in RightsIncludedPage
+
+  const rightsObjektArray = useAppSelector((state) => state.rightsIncludedPage.systemRegisterProductsArray);
+
+  const compact: boolean = false; // not used yet
+  const handleClick = () => {
+    console.log('handleClick');
+  }; // Copilot
+
+  
+  // not used in ActionBar: but Button might be added back later
+  const ButtonPlaceholder = () => {
+    return (<></>)
+  };
+
+  // used in CollectionBar below: only use two key:value pairs, but is extendable
+  // based on AccMan repo: /src/features/singleRight/components/ResourceCollectionBar
+  // Button probably will not be used: but might be added back later
+  const selectedRightsActionBars = rightsObjektArray.map((ProductRight, index) => (
+    <ActionBar
+      key={index}
+      title={ProductRight.right}
+      subtitle={ProductRight.serviceProvider}
+      size='small'
+      color='success'
+      actions={ButtonPlaceholder()}
+    ></ActionBar>
+  ));
+
+
+  // Note! CollectionBar has so far been hard-coded in component to
+  // say "Rettigheter ikke lagt til" --> now the opposite
+  // but should probably be responsive to whether collection input is empty
+
+  // added pilot rights collection ( from above, was [] before)
+  // Also note that each SystemUser now has the same 3 rights
+  // ---> must be linked to real rights of a given SystemUser
   const reduxCollectionBarArray = () => {
     return reduxObjektArray.map( (SystemUser) => (
       <div key={SystemUser.id}>
@@ -44,15 +86,13 @@ export const OverviewPageContent = () => {
           subtitle= { `${SystemUser.productName}` }
           additionalText= {""} 
           color={'neutral'}
-          collection={[]}
+          collection={selectedRightsActionBars}
           compact={isSm}
-          proceedToPath={ '/fixpath/' }
         />
       </div>
     ));
   };
   
-
   // Eldre greier: bør byttes ut, men kan trenges for Mobil-optimering
   const isSm = useMediaQuery('(max-width: 768px)'); // ikke i bruk lenger
 
@@ -64,12 +104,9 @@ export const OverviewPageContent = () => {
     navigate('/' + AuthenticationPath.Auth + '/' + AuthenticationPath.Creation);
   };
 
-
   return (
     <div className={classes.overviewPageContainer}>
-
       <h2 className={classes.pageContentText}>{overviewText}</h2>
-      
         <div className={classes.systemUserNewButton}>
           <Button
             variant='outline'
@@ -81,7 +118,6 @@ export const OverviewPageContent = () => {
             {t('authent_overviewpage.new_system_user_button')}
           </Button>
         </div>
-      
       <h2 className={classes.pageContentText}>
         {t('authent_overviewpage.existing_system_users_title')} 
       </h2>
