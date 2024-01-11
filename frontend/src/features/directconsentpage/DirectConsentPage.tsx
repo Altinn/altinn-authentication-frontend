06abbda2ed1e32cde18cd135d24be88b6cf8d80b
Torgeir Holen <94476391+Torgeir333@users.noreply.github.com>
2024-01-11 10:27:21 +0100

    DirectConsentPage creates mock systemUser upon Confirm (#144)
    * mock systemUser creation in DirectConsentPage
    
    * link up confirm button

--- frontend/src/features/directconsentpage/DirectConsentPage.tsx
+++ frontend/src/features/directconsentpage/DirectConsentPage.tsx
@@ -1,24 +1,17 @@
 import { useTranslation } from 'react-i18next';
 import * as React from 'react';
-
 import { Page, PageHeader, PageContent, PageContainer } from '@/components';
 import { ReactComponent as ApiIcon } from '@/assets/Api.svg';
 import { useMediaQuery } from '@/resources/hooks';
-
 import { DirectConsentPageContent } from './DirectConsentPageContent';
 
 export const DirectConsentPage = () => {
   const { t } = useTranslation('common');
   const isSm = useMediaQuery('(max-width: 768px)');
 
-  // fix-me: set language key in <PageHeader>
-
   return (
     <PageContainer>
-      <Page
-        color='dark'
-        size={isSm ? 'small' : 'medium'}
-      >
+      <Page color='dark' size={isSm ? 'small' : 'medium'} >
         <PageHeader icon={<ApiIcon />}>{t('authent_directconsentpage.banner_title')}</PageHeader>
         <PageContent>
           <DirectConsentPageContent />
