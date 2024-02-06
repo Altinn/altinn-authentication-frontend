c5a3221a590335a53745d511a70b2b8b9790fdfa
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-02-06 09:12:09 +0100

    still in draft

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemUsers/SystemUserService.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemUsers/SystemUserService.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
-namespace Altinn.Authentication.UI.Core.SystemUsers;
+﻿namespace Altinn.Authentication.UI.Core.SystemUsers;
 
 public class SystemUserService : ISystemUserService
 {
