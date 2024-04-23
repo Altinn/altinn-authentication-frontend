9cab392a24e253cc3e65c09727f3926c07743ceb
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-04-23 10:16:14 +0200

    puttet defaultrights inni dto

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/SystemRegisterClient.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/SystemRegisterClient.cs
@@ -12,28 +12,48 @@ public class SystemRegisterClient : ISystemRegisterClient
         {
             SystemTypeId = "4human_hr_system_2024_2",
             SystemVendor = "4Human",
-            Description = "4Humans HR system 2024 versjon."
+            Description = "4Humans HR system 2024 versjon.",
+            DefaultRights =
+            [
+                new DefaultRightsDTO { Right = "Sykemelding - Oppgi leder", ServiceProvider = "Arbeids- og velferdsetaten (NAV)" },
+                new DefaultRightsDTO { Right = "Søknad om sykepenger", ServiceProvider = "Arbeids- og velferdsetaten (NAV)" }
+            ]
+            
         };
 
         RegisteredSystemDTO regsys2 = new()
         {
             SystemTypeId = "din_lokale_regnskapspartner",
             SystemVendor = "Din Lokale Regnskapspartner AS",
-            Description  = "Regnskap og Revisor tjenester"
+            Description  = "Regnskap og Revisor tjenester",
+            DefaultRights =
+            [
+                new DefaultRightsDTO { Right = "MVA rapportering", ServiceProvider = "Skatteetaten" },
+                new DefaultRightsDTO { Right = "Årsregnskap", ServiceProvider = "Skatteetaten" }
+            ]
         };
 
         RegisteredSystemDTO regsys3 = new()
         {
             SystemTypeId = "fiken_smabedrift",
             SystemVendor = "Fiken",
-            Description =  "Fiken Småbedrift pakken"
+            Description =  "Fiken Småbedrift pakken",
+            DefaultRights =
+            [
+                new DefaultRightsDTO { Right = "MVA rapportering", ServiceProvider = "Skatteetaten" },
+                new DefaultRightsDTO { Right = "Årsregnskap", ServiceProvider = "Skatteetaten" }
+            ]
         };
 
         RegisteredSystemDTO regsys4 = new()
         {
             SystemTypeId = "visma_mva_pakke",
             SystemVendor ="Visma",
-            Description = "Visma MVA rapportering"
+            Description = "Visma MVA rapportering",
+            DefaultRights =
+            [
+                new DefaultRightsDTO { Right = "MVA rapportering", ServiceProvider = "Skatteetaten" }             
+            ]
 
         };
 
@@ -41,7 +61,14 @@ public class SystemRegisterClient : ISystemRegisterClient
         {
             SystemTypeId = "visma_skatt_totalpakke",
             SystemVendor ="Visma",
-            Description = "Visma Totalpakke for alle skatterapporterings behov"
+            Description = "Visma Totalpakke for alle skatterapporterings behov",
+            DefaultRights =
+            [
+                new DefaultRightsDTO { Right = "MVA rapportering", ServiceProvider = "Skatteetaten" },
+                new DefaultRightsDTO { Right = "Årsregnskap", ServiceProvider = "Skatteetaten" },
+                new DefaultRightsDTO { Right = "Bytte av Revisor", ServiceProvider = "Skatteetaten" },
+                new DefaultRightsDTO { Right = "Levere Lakselus", ServiceProvider = "Mattilsynet" },
+            ]
         };
 
         List<RegisteredSystemDTO> theList = new()
