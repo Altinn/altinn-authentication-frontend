1d71a464a5bf0fecb01d5721bf45f4867998310d
acn-dgopa <acn-dgopa@dev-acn-tje-14>
2024-04-23 14:21:58 +0200

    retreive and send xsrf token

--- frontend/src/rtk/features/api.ts
+++ frontend/src/rtk/features/api.ts
@@ -1,8 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { getCookie } from '@/resources/Cookie/CookieMethods';
 
 // Define a service using a base URL and expected endpoints
 export const api = createApi({
   reducerPath: 'systemUserApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '/authfront/api/v1/' }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: '/authfront/api/v1/',
+    prepareHeaders: (headers) => {
+      headers.set('content-type', 'application/json; charset=utf-8');
+      headers.set('X-XSRF-TOKEN', getCookie('XSRF-TOKEN'));
+      return headers;
+    },
+  }),
   endpoints: () => ({}),
 });
