641a3c784f69ab31f49a79e7c91c065f26c8f226
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-04-25 20:00:03 +0200

    removed wrong constructors

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/AppConfiguration/ClientSettings.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/AppConfiguration/ClientSettings.cs
@@ -2,15 +2,6 @@
 
 public class ClientSettings
 {
-    public ClientSettings() { }
-
-    public ClientSettings(string issuer, string app, string certificateName)
-    {
-        Issuer = issuer;
-        App = app;
-        CertificateName = certificateName;
-    }
-
     public string Issuer { get; set; }
 
     public string App { get; set; }
