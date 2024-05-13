30561d62194f89164108f201e46efa662139873e
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-05-13 09:12:38 +0200

    language stuff

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/Common/Models/LanguageSupport.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Authentication.UI.Core.Common.Models
+{
+    /// <summary>
+    /// Supports language options for Text fields
+    /// </summary>
+    public record LanguageSupport
+    {
+        /// <summary>
+        /// Contains the Text in a version for each language in a ValuePair list.
+        /// Id for the language name : "en", "nb", "nn"
+        /// Value holds the text version for the chosen language
+        /// </summary>
+        public List<ValuePair> Text { get; init; } = [];
+
+
+    }
+}
---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/Common/Models/ValuePair.cs
@@ -0,0 +1,19 @@
+﻿namespace Altinn.Authentication.UI.Core.Common.Models
+
+{
+    /// <summary>
+    /// Simple Id/Value pair using strings for both properties
+    /// </summary>
+    public record ValuePair
+    {
+        /// <summary>
+        /// The Id, accepts any string
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The Value, string or string formatted
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
