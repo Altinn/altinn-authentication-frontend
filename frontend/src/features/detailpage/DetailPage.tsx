93df02bf719ecdbec2da5524fe0f28ce07d6673e
Martin Gunnerud <mgunnerud@gmail.com>
2024-05-22 16:04:32 +0200

    show spinner when loading systemuser

--- frontend/src/features/detailpage/DetailPage.tsx
+++ frontend/src/features/detailpage/DetailPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Alert } from '@digdir/designsystemet-react';
+import { Alert, Spinner } from '@digdir/designsystemet-react';
 import { Page, PageContainer } from '@/components';
 import { DetailPageContent } from './DetailPageContent';
 import ApiIcon from '@/assets/Api.svg?react';
@@ -9,11 +9,16 @@ import { useGetSystemUserQuery } from '@/rtk/features/systemUserApi';
 export const DetailPage = (): React.ReactNode => {
   const { id } = useParams();
 
-  const { data: systemUser, isError: isLoadSystemUserError } = useGetSystemUserQuery(id || '');
+  const {
+    data: systemUser,
+    isError: isLoadSystemUserError,
+    isLoading: isLoadingSystemUser,
+  } = useGetSystemUserQuery(id || '');
 
   return (
     <PageContainer>
       <Page color='dark' icon={<ApiIcon />} title={'Rediger systemtilgang'}>
+        {isLoadingSystemUser && <Spinner title='laster systemtilgang' />}
         {isLoadSystemUserError && <Alert severity='danger'>Kunne ikke laste systemtilgang</Alert>}
         {systemUser && <DetailPageContent systemUser={systemUser} />}
       </Page>
