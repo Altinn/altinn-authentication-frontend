bd1f9d8d9ab5b40c2a6cef25579354e475c7cb28
Martin Gunnerud <mgunnerud@gmail.com>
2024-05-23 08:52:47 +0200

    add back button + change styling of close button

--- frontend/src/components/PageContainer/PageContainer.tsx
+++ frontend/src/components/PageContainer/PageContainer.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { ArrowLeftIcon } from '@navikt/aksel-icons';
+import { Button, Tooltip } from '@digdir/designsystemet-react';
 import CloseIcon from '@/assets/RedClose.svg?react';
 import { getCookie } from '@/resources/Cookie/CookieMethods';
 import { UserInfoBar } from '../UserInfoBar/UserInfoBar';
 import classes from './PageContainer.module.css';
 
 export interface PageContainerProps {
+  backUrl?: string;
   children: React.ReactNode;
 }
 
-export const PageContainer = ({ children }: PageContainerProps) => {
+export const PageContainer = ({ backUrl, children }: PageContainerProps) => {
   const { t } = useTranslation();
 
   const redirectToProfile = () => {
@@ -21,14 +25,34 @@ export const PageContainer = ({ children }: PageContainerProps) => {
     <div className={classes.pageMargin}>
       <div className={classes.pageContainer}>
         <UserInfoBar />
-        <div className={classes.closeButtonContainer}>
-          <button
-            className={classes.closeButton}
-            aria-label={String(t('common.close'))}
-            onClick={redirectToProfile}
-          >
-            <CloseIcon className={classes.closeButtonIcon} />
-          </button>
+        <div className={classes.buttonContainer}>
+          {backUrl && (
+            <Tooltip content='Tilbake'>
+              <Button
+                icon={true}
+                color='second'
+                variant='tertiary'
+                className={classes.buttonContainerButton}
+                size='small'
+                asChild
+              >
+                <RouterLink to={backUrl}>
+                  <ArrowLeftIcon className={classes.buttonContainerIcon} />
+                </RouterLink>
+              </Button>
+            </Tooltip>
+          )}
+          <Tooltip content={t('common.close')}>
+            <Button
+              icon={true}
+              variant='tertiary'
+              className={classes.buttonContainerButton}
+              size='small'
+              onClick={redirectToProfile}
+            >
+              <CloseIcon className={classes.buttonContainerIcon} />
+            </Button>
+          </Tooltip>
         </div>
         <div>{children}</div>
       </div>
--- frontend/src/features/creationpage/CreationPage.tsx
+++ frontend/src/features/creationpage/CreationPage.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import { Page, PageContainer } from '@/components';
 import ApiIcon from '@/assets/Api.svg?react';
 import { CreationPageContent } from './CreationPageContent';
+import { AuthenticationRoute } from '@/routes/paths';
 
 export const CreationPage = () => {
   const { t } = useTranslation();
 
   return (
-    <PageContainer>
+    <PageContainer backUrl={AuthenticationRoute.Overview}>
       <Page color='dark' icon={<ApiIcon />} title={t('authent_creationpage.banner_title')}>
         <CreationPageContent />
       </Page>
