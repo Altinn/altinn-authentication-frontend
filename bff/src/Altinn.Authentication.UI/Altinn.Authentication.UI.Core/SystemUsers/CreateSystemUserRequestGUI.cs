6cba3a97884da539a6b127665e76bb4b39904284
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-08-08 13:37:11 +0200

    GUI DTO

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemUsers/CreateSystemUserRequestGUI.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemUsers/CreateSystemUserRequestGUI.cs
@@ -4,24 +4,12 @@ namespace Altinn.Authentication.UI.Core.SystemUsers;
 
 public sealed record CreateSystemUserRequestGUI
 {
-    [JsonPropertyName("partyId")]
-    /// <summary>
-    /// PartyId is the owning Legal Entity, whether an organization or a privat person
-    /// </summary>
-    public int OwnedByPartyId { get; set; }
-
-    /// <summary>
-    /// The Organisation Number for the end-user as it is stored in ER Registry        
-    /// </summary>
-    [JsonPropertyName("reporteeOrgNo")]
-    public string ReporteeOrgNo { get; set; }
-
     /// <summary>
     /// The Title is set by the end-user in the Frontend, by default it is the same as the System's Display Name
     /// Even if this DTO allows null, the db field is of course still required   
     /// </summary>
     [JsonPropertyName("integrationTitle")]
-    public string IntegrationTitle { get; set; }
+    public string? IntegrationTitle { get; set; }
 
     /// <summary>
     /// For off the shelf systems.
@@ -29,6 +17,6 @@ public sealed record CreateSystemUserRequestGUI
     /// The "real" Authentication Component should validate that the SystemName is unique
     /// Retrieved from the SystemRegister, the full CRUD Api is in a different service
     /// </summary>
-    [JsonPropertyName("systemId")]
-    public string SelectedSystemType { get; set; }
+    [JsonPropertyName("selectedSystemType")]
+    public string? SelectedSystemType { get; set; }
 }
