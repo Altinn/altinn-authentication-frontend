27de3f0ab93896688e4998fa06475c0ae3b26556
Martin Gunnerud <mgunnerud@gmail.com>
2024-08-29 09:58:25 +0200

    casing??

--- frontend/playwright/playwright.config.ts
+++ frontend/playwright/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
       },
     ],
   ],
-  outputDir: `frontend/playwright/test-results/${process.env.environment?.toLowerCase() ?? 'at22'}`,
+  outputDir: `frontend/playwright/test-results/${process.env.environment?.toUpperCase() ?? 'at22'}`,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
