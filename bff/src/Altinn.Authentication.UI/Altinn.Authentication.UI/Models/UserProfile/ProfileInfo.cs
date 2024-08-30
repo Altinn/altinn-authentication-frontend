030c014447980a9bff9d361d783b834866d5ae9c
Martin Gunnerud <mgunnerud@gmail.com>
2024-08-30 14:19:55 +0200

    only allow users with one of DAGL, HADM or ADMAI for organization to create system user

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Models/UserProfile/ProfileInfo.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI/Models/UserProfile/ProfileInfo.cs
@@ -4,4 +4,5 @@ public sealed record ProfileInfo
 {
     public string? LoggedInPersonName { get; set; }
     public string? RepresentingPartyName { get; set; }
+    public bool? CanCreateSystemUser { get; set; }
 }
