11c9022061caa68547aa7eb569d7858afeed732d
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-09 13:01:16 +0200

    fixed 2 tests

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Mocks/Mocks/SystemUsers/SystemUserClientMock.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Mocks/Mocks/SystemUsers/SystemUserClientMock.cs
@@ -5,51 +5,58 @@ namespace Altinn.Authentication.UI.Mocks.SystemUsers;
 
 public class SystemUserClientMock : ISystemUserClient
 {
-    private static List<SystemUserReal> MockTestHelper()
+    private static List<SystemUser> MockTestHelper()
     {
         //Mock Data
-        SystemUserReal systemUser1 = new()
+        SystemUser systemUser1 = new()
         {
             Id = "37ce1792-3b35-4d50-a07d-636017aa7dbd",
-            Title = "Vårt regnskapsystem",
-            Description = "Koblet opp mot Visma. Snakk med Pål om abonnement",
-            SystemType = "visma_vis_v2",
-            OwnedByPartyId = "orgno:91235123",
-            Created = "2023-09-12",
+            IntegrationTitle = "Vårt regnskapsystem",
+            SystemId = "987654321_regnvær_regnskap_system",
+            PartyId = "91235123",
+            Created = System.DateTime.UtcNow,
             IsDeleted = false,
-            ClientId = ""
+            ReporteeOrgNo = "123456789",
+            SupplierName = "Regnvær AS",
+            SupplierOrgNo = "987654321",
+            SystemInternalId = Guid.NewGuid(),
         };
 
-        SystemUserReal systemUser2 = new()
+        SystemUser systemUser2 = new()
         {
             Id = "37ce1792-3b35-4d50-a07d-636017aa7dbe",
-            Title = "Vårt andre regnskapsystem",
-            Description = "Snakk med Per om abonnement",
-            SystemType = "visma_vis_sys",
-            OwnedByPartyId = "orgno:91235124",
-            Created = "2023-09-22",
+            IntegrationTitle = "Vårt regnskapsystem",
+            SystemId = "987654322_regnfullhøst_regnskap_system",
+            PartyId = "91235123",
+            Created = System.DateTime.UtcNow,
             IsDeleted = false,
-            ClientId = ""
+            ReporteeOrgNo = "123456789",
+            SupplierName = "Regnfull Høst ASA",
+            SupplierOrgNo = "987654322",
+            SystemInternalId = Guid.NewGuid(),
         };
 
-        SystemUserReal systemUser3 = new()
+        SystemUser systemUser3 = new()
         {
             Id = "37ce1792-3b35-4d50-a07d-636017aa7dbf",
-            Title = "Et helt annet system",
-            Description = "Kai og Guri vet alt om dette systemet.",
-            SystemType = "fiken_superskatt",
-            OwnedByPartyId = "orgno:91235125",
-            Created = "2023-09-22",
+            IntegrationTitle = "Vårt regnskapsystem",
+            SystemId = "987654323_regnskog_regnskap_system",
+            PartyId = "91235123",
+            Created = System.DateTime.UtcNow,
             IsDeleted = false,
-            ClientId = ""
+            ReporteeOrgNo = "123456789",
+            SupplierName = "Regnskog AS",
+            SupplierOrgNo = "987654323",
+            SystemInternalId = Guid.NewGuid(),
         };
 
-    List<SystemUserReal> systemUserList = new()
-        {
+    List<SystemUser> systemUserList =
+        [
             systemUser1,
             systemUser2,
             systemUser3
-        };
+        ];
+
         return systemUserList;
     }
 
@@ -79,7 +86,7 @@ public class SystemUserClientMock : ISystemUserClient
     {
         _partyClient = partyClient;
         _httpClient = httpClient;
-        _systemUserList = MockTestHelperNew();
+        _systemUserList = MockTestHelper();
     }
    
     public async Task<SystemUser?> GetSpecificSystemUserReal(int partyId, Guid id, CancellationToken cancellationToken = default)
