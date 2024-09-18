f7f2fd5faf746413e11d99d807f62e2c0bf33358
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-18 17:40:28 +0200

    init client

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemUsers/RequestClient.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemUsers/RequestClient.cs
@@ -14,19 +14,21 @@ namespace Altinn.Authentication.UI.Integration.SystemUsers;
 public class RequestClient(
     HttpClient client,
     IOptions<PlatformSettings> optionsPlatformSetting,
-    IHttpContextAccessor httpContext,
-    IAccessTokenProvider accessTokenProvider
+    IHttpContextAccessor httpContext
     ) : IRequestClient
 {    
 
-    public async Task<Result<VendorRequest>> GetVendorRequest(int partyId, Guid requestId)
+    private string InitClient()
     {
         client.BaseAddress = new(optionsPlatformSetting.Value.ApiAuthenticationEndpoint!);
         client.DefaultRequestHeaders.Add(optionsPlatformSetting.Value.SubscriptionKeyHeaderName, optionsPlatformSetting.Value.SubscriptionKey);
-        string token = JwtTokenUtil.GetTokenFromContext(httpContext.HttpContext!, optionsPlatformSetting.Value.JwtCookieName!)!;
-        string endpoint = $"systemuser/request/{partyId}/{requestId}";
+        return JwtTokenUtil.GetTokenFromContext(httpContext.HttpContext!, optionsPlatformSetting.Value.JwtCookieName!)!;
+    }
 
-        HttpResponseMessage res = await client.GetAsync(token, endpoint);
+    public async Task<Result<VendorRequest>> GetVendorRequest(int partyId, Guid requestId)
+    {        
+        string endpoint = $"systemuser/request/{partyId}/{requestId}";
+        HttpResponseMessage res = await client.GetAsync(InitClient(), endpoint);
 
         if (res.IsSuccessStatusCode)
         {
