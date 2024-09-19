d083d8979e528ee0b5f126bdc8df1807fc1d8d7a
Simen Rekkedal <61084786+simen-rekkedal@users.noreply.github.com>
2024-09-19 12:56:04 +0200

    L2 get vendor request (#372)
    * init branch, Request Controller
    
    * service and client
    
    * init client
    
    * bugfix
    
    * housekeeping
    
    * comment fix

---
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Core/SystemUsers/RequestService.cs
@@ -0,0 +1,17 @@
+﻿using Altinn.Authorization.ProblemDetails;
+
+namespace Altinn.Authentication.UI.Core.SystemUsers;
+
+/// <summary>
+/// Request Service
+/// </summary>
+/// <param name="requestClient">The client</param>
+internal class RequestService(
+    IRequestClient requestClient
+    ) : IRequestService
+{
+    public Task<Result<VendorRequest>> GetVendorRequest(int partyId, Guid requestId)
+    {
+        return requestClient.GetVendorRequest(partyId, requestId);
+    }
+}
