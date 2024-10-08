ea5e83cd84a20989cf2aa1553fe40ed82cd83dd4
Martin Gunnerud <mgunnerud@gmail.com>
2024-10-08 13:30:16 +0200

    test api change

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/SystemRegisterClient.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/SystemRegisterClient.cs
@@ -71,7 +71,7 @@ public class SystemRegisterClient : ISystemRegisterClient
     public async Task<List<Right>> GetRightsFromSystem(string systemId, CancellationToken cancellationToken = default)
     {
         string token = JwtTokenUtil.GetTokenFromContext(_httpContextAccessor.HttpContext!, _platformSettings.JwtCookieName!)!;
-        string endpointUrl = $"systemregister/{systemId}/rights";
+        string endpointUrl = $"systemregister/system/{systemId}/rights";
         var accessToken = await _accessTokenProvider.GetAccessToken();
 
         HttpResponseMessage response = await _httpClient.GetAsync(token, endpointUrl, accessToken);
