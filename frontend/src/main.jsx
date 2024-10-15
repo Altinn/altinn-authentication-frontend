7f20fd174a5d271016e9a136027b5a54b6c1b9d0
Martin Gunnerud <mgunnerud@gmail.com>
2024-10-15 10:41:52 +0200

    add msw + mocks for requests

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -29,10 +29,24 @@ use(LanguageDetector)
     interpolation: { escapeValue: false },
   });
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={Router}></RouterProvider>
-    </Provider>
-  </StrictMode>,
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== 'development') {
+    return;
+  }
+
+  const { worker } = await import('./mocks/browser');
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={Router}></RouterProvider>
+      </Provider>
+    </StrictMode>,
+  );
+});
