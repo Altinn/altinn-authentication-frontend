855e006d6873efda73dd879014de49003ab35d7f
Martin Gunnerud <mgunnerud@gmail.com>
2024-10-27 21:51:45 +0100

    add memory cache + cache resource owner list

--- bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/ResourceRegistryClient.cs
+++ bff/src/Altinn.Authentication.UI/Altinn.Authentication.UI.Integration/SystemRegister/ResourceRegistryClient.cs
@@ -5,6 +5,8 @@ using System.Text.Json;
 using Microsoft.Extensions.Options;
 using System.Net.Http.Json;
 using Altinn.Authentication.UI.Core.Common.Rights;
+using Microsoft.Extensions.Caching.Memory;
+using Altinn.Authentication.UI.Core.AppConfiguration;
 
 namespace Altinn.Authentication.UI.Integration.SystemRegister;
 
@@ -13,17 +15,23 @@ public class ResourceRegistryClient : IResourceRegistryClient
     private readonly ILogger _logger;
     private readonly HttpClient _httpClient;
     private readonly PlatformSettings _platformSettings;
+    private readonly IMemoryCache _memoryCache;
+    private readonly CacheConfig _cacheConfig;
     private readonly JsonSerializerOptions _jsonSerializerOptions = new() { PropertyNameCaseInsensitive = true };
 
     public ResourceRegistryClient(
         ILogger<SystemRegisterClient> logger,
         HttpClient httpClient,
-        IOptions<PlatformSettings> platformSettings)
+        IOptions<PlatformSettings> platformSettings,
+        IMemoryCache memoryCache,
+        IOptions<CacheConfig> cacheConfig)
     {
         _logger = logger;
         _platformSettings = platformSettings.Value;
         httpClient.BaseAddress = new Uri(_platformSettings!.ResourceRegistryEndpoint);
         _httpClient = httpClient;
+        _memoryCache = memoryCache;
+        _cacheConfig = cacheConfig.Value;
     }
 
     public async Task<List<ServiceResource>> GetResources(List<Right> rights, CancellationToken cancellationToken = default)
@@ -78,9 +86,7 @@ public class ResourceRegistryClient : IResourceRegistryClient
 
     private async Task<string?> GetOrgIconUrl(string org, CancellationToken cancellationToken)
     {
-        // TODO: cache org list
-        HttpResponseMessage response = await _httpClient.GetAsync("https://altinncdn.no/orgs/altinn-orgs.json", cancellationToken);
-        OrgList? orgList = await response.Content.ReadFromJsonAsync<OrgList>(_jsonSerializerOptions, cancellationToken);
+        OrgList? orgList = await GetAllResourceOwners(cancellationToken);
         if (orgList != null)
         {
             if (orgList.Orgs.TryGetValue(org, out Org? organization))
@@ -90,5 +96,32 @@ public class ResourceRegistryClient : IResourceRegistryClient
         }
         
         return null;
+    }
+
+    private async Task<OrgList?> GetAllResourceOwners(CancellationToken cancellationToken)
+    {
+        string endpointUrl = "resource/orgs";
+        string cacheKey = "all_resource_owners";
+        if (!_memoryCache.TryGetValue(cacheKey, out OrgList? resourceOwners))
+        {
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(endpointUrl, cancellationToken);
+                resourceOwners = await response.Content.ReadFromJsonAsync<OrgList>(_jsonSerializerOptions, cancellationToken);
+                MemoryCacheEntryOptions cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetPriority(CacheItemPriority.High)
+                    .SetAbsoluteExpiration(new TimeSpan(0, _cacheConfig.ResourceOwnerCacheTimeout, 0));
+
+                _memoryCache.Set(cacheKey, resourceOwners, cacheEntryOptions);
+                return resourceOwners;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Authentication.UI // ResourceClient // GetAllResourceOwners // Exception");
+                throw;
+            }
+        }
+
+        return resourceOwners;
     }
 }
