18c6dec4b257efbe671cc45882810ef354f7f5aa
Martin Gunnerud <mgunnerud@gmail.com>
2024-11-20 12:28:21 +0100

    add translations for delegation errors

--- frontend/src/components/DelegationCheckError/DelegationCheckError.tsx
+++ frontend/src/components/DelegationCheckError/DelegationCheckError.tsx
@@ -16,11 +16,23 @@ export const DelegationCheckError = ({ error }: DelegationCheckErrorProps): Reac
   const getErrorMessage = (): string => {
     switch (error?.data?.code) {
       case 'ATUI-00001':
-        return 'One or more Right not found or not delegable.';
+        return t('01_rights_not_found_or_not_delegable');
       case 'ATUI-00002':
-        return 'The Delegation failed.';
+        return t('02_rights_failed_to_delegate');
       case 'ATUI-00003':
-        return 'Failed to create the SystemUser.';
+        return t('03_systemuser_failed_to_create');
+      case 'ATUI-00004':
+        return t('04_system_user_already_exists');
+      case 'ATUI-00014':
+        return t('14_unable_to_do_delegation_check');
+      case 'ATUI-00015':
+        return t('15_delegation_right_missing_role_access');
+      case 'ATUI-00016':
+        return t('16_delegation_right_missing_delegation_access');
+      case 'ATUI-00017':
+        return t('17_delegation_right_missing_srr_right_access');
+      case 'ATUI-00018':
+        return t('18_delegation_right_insufficient_authentication_level');
       default:
         return t('authent_includedrightspage.create_systemuser_error');
     }
