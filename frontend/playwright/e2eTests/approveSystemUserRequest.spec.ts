54af1c89c5ee25f300bf2f8b7c951ca20f08642f
Vegard Wright <vegard.nyeng@gmail.com>
2024-12-11 16:52:47 +0100

    update playwright tests to verify status on system user requests (#453)

--- frontend/playwright/e2eTests/approveSystemUserRequest.spec.ts
+++ frontend/playwright/e2eTests/approveSystemUserRequest.spec.ts
@@ -16,25 +16,33 @@ test.describe('Godkjenn og avvis Systembrukerforespørsel', () => {
 
   test('Avvis Systembrukerforespørsel', async ({ page }): Promise<void> => {
     //Generate confirmUrl from API
-    const confirmUrl = await prepareSystemUserRequest(api, token);
+    const systemUserRequestResponse = await prepareSystemUserRequest(api, token);
 
-    await page.goto(confirmUrl);
+    await page.goto(systemUserRequestResponse.confirmUrl);
     await page.getByRole('button', { name: 'Avvis' }).click();
 
     //Expect user to be logged out
     await expect(loginPage.LOGIN_BUTTON).toBeVisible();
     await expect(page).toHaveURL('https://info.altinn.no');
+
+    //Read from status api to verify that status is not rejected after clicking "Avvis"
+    const statusApiRequest = await getStatusForRequestApi(systemUserRequestResponse.id, token);
+    expect(statusApiRequest.status).toBe('Rejected');
   });
 
   test('Godkjenn Systembrukerforespørsel', async ({ page }): Promise<void> => {
-    const confirmUrl = await prepareSystemUserRequest(api, token);
+    const systemUserRequestResponse = await prepareSystemUserRequest(api, token);
 
-    await page.goto(confirmUrl);
+    await page.goto(systemUserRequestResponse.confirmUrl);
     await page.getByRole('button', { name: 'Godkjenn' }).click();
 
     //Expect user to be logged out
     await expect(loginPage.LOGIN_BUTTON).toBeVisible();
     await expect(page).toHaveURL('https://info.altinn.no');
+
+    //Read from status api to verify that status is not Accepted after clicking "Avvis"
+    const statusApiRequest = await getStatusForRequestApi(systemUserRequestResponse.id, token);
+    expect(statusApiRequest.status).toBe('Accepted');
   });
 
   async function prepareSystemUserRequest(api: ApiRequests, tokenclass: Token) {
@@ -43,7 +51,19 @@ test.describe('Godkjenn og avvis Systembrukerforespørsel', () => {
       'altinn:authentication/systemuser.request.read altinn:authentication/systemuser.request.write';
     const token = await tokenclass.getEnterpriseAltinnToken(scopes);
     const endpoint = 'v1/systemuser/request/vendor';
-    const apiResponse = await api.sendPostRequest<{ confirmUrl: string }>(payload, endpoint, token);
-    return apiResponse.confirmUrl; // Return the Confirmation URL to use in the test
+    const apiResponse = await api.sendPostRequest<{ confirmUrl: string; id: string }>(
+      payload,
+      endpoint,
+      token,
+    );
+    return { confirmUrl: apiResponse.confirmUrl, id: apiResponse.id }; // Return the Confirmation URL and the ID to use in the test
+  }
+
+  async function getStatusForRequestApi(id: string, tokenclass: Token) {
+    const scopes =
+      'altinn:authentication/systemuser.request.read altinn:authentication/systemuser.request.write';
+    const token = await tokenclass.getEnterpriseAltinnToken(scopes);
+    const statusResponse = api.getStatusForSystemUserRequest<{ status: string }>(token, id);
+    return statusResponse;
   }
 });
