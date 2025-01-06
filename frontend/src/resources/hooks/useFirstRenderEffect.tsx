c47194a0f511be51609179d2d64c59eecde53c04
Martin Gunnerud <mgunnerud@gmail.com>
2025-01-06 14:00:37 +0100

    remove dead code

--- frontend/src/resources/hooks/useFirstRenderEffect.tsx
+++
@@ -1,5 +0,0 @@
-import { useEffect } from 'react';
-
-export const useFirstRenderEffect = (effectFn: () => void) => {
-  useEffect(effectFn, []); // eslint-disable-line react-hooks/exhaustive-deps
-};
