aed25e89320fe21ed977caeda3c7e9accfbb61bb
Vegard Wright <vegard.nyeng@gmail.com>
2025-02-05 14:57:14 +0100

    proper cleanup of systems for Playwright tests (#485)

--- frontend/playwright/e2eTests/selectSystemVendor.spec.ts
+++ frontend/playwright/e2eTests/selectSystemVendor.spec.ts
@@ -3,19 +3,26 @@ import { SystemUserPage } from '../pages/systemUserPage';
 import { TestdataApi } from 'playwright/util/TestdataApi';
 import { ApiRequests } from 'playwright/api-requests/ApiRequests';
 
-test('Create system user and verify landing page', async ({ page }): Promise<void> => {
-  //Make sure system user does not exist first
-  const api: ApiRequests = new ApiRequests();
 
-  await TestdataApi.removeAllSystemUsers();
-  const system = await api.createSystemSystemRegister();
+test.describe('System Register', async () => {
+  let system: string;
 
-  const systemUserPage = new SystemUserPage(page);
-  await systemUserPage.selectSystem(system);
-  await systemUserPage.CREATE_SYSTEM_USER_BUTTON.click();
-  await expect(systemUserPage.SYSTEMUSER_CREATED_HEADING).toBeVisible();
-  await expect(page.getByText(system).first()).toBeVisible();
+  test.beforeEach(async () => {
+    const api = new ApiRequests();
+    system = await api.createSystemSystemRegister(); // Create system before each test
+  });
 
-  //Cleanup
-  await TestdataApi.removeSystem(system);
+  test('Create system user and verify landing page', async ({ page }): Promise<void> => {
+    const systemUserPage = new SystemUserPage(page);
+    await systemUserPage.selectSystem(system);
+    await systemUserPage.CREATE_SYSTEM_USER_BUTTON.click();
+    await expect(systemUserPage.SYSTEMUSER_CREATED_HEADING).toBeVisible();
+    await expect(page.getByText(system).first()).toBeVisible();
+  });
+
+  test.afterEach(async () => {
+    if (system) {
+      await TestdataApi.removeSystem(system);
+    }
+  });
 });
